99b7fd43e2b8c2a12f40570949ccda1dfaf3b341
eshenxd <shenxd1989@163.com>
2014-10-22 23:01:21 +0800

    facesDetector
    code to detect faces

---
+++ header.h
@@ -0,0 +1,28 @@
+#include <atlimage.h>
+#include <time.h>
+#include <math.h>
+#include <string>
+#include <sstream>
+#include "io.h"
+#include <vector>
+#include <iostream>
+
+#include<Windows.h>
+
+
+
+#include "../opencv/include/opencv/cv.h"
+#include "../opencv/include/opencv/highgui.h"
+#include "../opencv/include/opencv/cxcore.h"
+
+//Debug
+#pragma comment(lib, "../opencv/lib/opencv_core246d.lib")
+#pragma comment(lib, "../opencv/lib/opencv_objdetect246d.lib")
+#pragma comment(lib, "../opencv/lib/opencv_highgui246d.lib")
+#pragma comment(lib, "../opencv/lib/opencv_imgproc246d.lib")
+
+//Release
+#pragma comment(lib, "../opencv/lib/opencv_core246.lib")
+#pragma comment(lib, "../opencv/lib/opencv_objdetect246.lib")
+#pragma comment(lib, "../opencv/lib/opencv_highgui246.lib")
+#pragma comment(lib, "../opencv/lib/opencv_imgproc246.lib")
